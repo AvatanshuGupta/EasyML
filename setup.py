6e05eb9391a70d5eedf2095c117a7c5a3a5a9e86
AvatanshuGupta <avatanshugupta@gmail.com>
2025-08-22 16:34:43 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='EasyML',                  # Package name (unique on PyPI)
-    version='0.1.0',                 # Initial version
-    author='Avatanshu Gupta',        # Your name
+    name='Neurix',                 
+    version='0.1.0',                
+    author='Avatanshu Gupta',        
     author_email='avatanshugupta@gmail.com',  
     description='EasyMl is a simple ML utility library with preprocessing, regression, classification, and evaluation tools',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/AvatanshuGupta/EasyML.git',  # Optional: GitHub repo
-    packages=find_packages(),         # Automatically finds all packages
-    python_requires='>=3.8',         # Minimum Python version
-    install_requires=[               # Dependencies
+    url='https://github.com/AvatanshuGupta/EasyML.git', 
+    packages=find_packages(),       
+    python_requires='>=3.8',      
+    install_requires=[            
         'numpy'
         
     ],
-    classifiers=[                    # Optional, helps PyPI categorize your package
+    classifiers=[                   
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
 )
